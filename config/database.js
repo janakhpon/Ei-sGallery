7a7f2552aec8da7670414bf60d75b04957fb3438
janakhpon <jnovaxer@gmail.com>
2019-05-17 16:32:06 +0630

    URI

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
   module.exports = {
-    mongoURI:
-      "mongodb://noteuser:noteuser1998@ds125693.mlab.com:25693/projecttimetable"
+    mongoURI: process.env.MONGO_URI
+      
   };
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/Galarie'}
